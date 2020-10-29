5396a0f6b5eb3de9c71575d706c98c176caf04e7
Denis Moiseev <dnsmsv@gmail.com>
2020-10-29 16:07:49 +0300

    Scroll to bottom

--- src/app/chatroom/chatroom.component.ts
+++ src/app/chatroom/chatroom.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  OnInit,
+  ViewChild,
+  AfterViewChecked,
+} from '@angular/core';
 import { Router } from '@angular/router';
 import { AuthService } from '../services/auth.service';
 
@@ -7,12 +13,22 @@ import { AuthService } from '../services/auth.service';
   templateUrl: './chatroom.component.html',
   styleUrls: ['./chatroom.component.css'],
 })
-export class ChatroomComponent implements OnInit {
+export class ChatroomComponent implements OnInit, AfterViewChecked {
+  @ViewChild('scroller') private feedContainer: ElementRef;
+
   constructor(private authService: AuthService, private router: Router) {}
 
   ngOnInit(): void {
     if (!this.authService.user) {
       this.router.navigate(['login']);
     }
+  }
+
+  scrollToBottom(): void {
+    this.feedContainer.nativeElement.scrollTop = this.feedContainer.nativeElement.scrollHeight;
+  }
+
+  ngAfterViewChecked() {
+    this.scrollToBottom();
   }
 }
